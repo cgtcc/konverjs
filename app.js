a6fc8f832a34902ef52fbede7a30415b7ebecd0f
codecoaster <thecodecoaster@gmail.com>
2017-03-06 16:01:34 -0500

    middleware for determining if the user is authenticated

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use(passport.session());
 //Middleware for determining if the user is authenticated
 //it's important to place this function before loading the routes, so every routes will inhert from this Middleware
 
-function ensureAuthenticated(req, res, next){
+function amIauthenticated(req, res, next){
   if (req.isAuthenticated()) {  //this function is provided by passport.  Makes our life easier, and safer.
     next();
   } else {
